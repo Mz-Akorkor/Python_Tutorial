83239288da6e765f49e5ad05c77ec1ceb7b3c89f
bright-settor <brightdelasettor@gmail.com>
2025-07-15 13:29:46 +0000

    changes to numbers file

---
+++ numbers_exercises_bright.py
@@ -0,0 +1,100 @@
+# 🔢 Basic Number Exercises
+
+# Create a variable x with value 15 and y with value 4.
+
+x = 15
+y = 4
+
+# Print their sum, difference, product, and quotient.
+
+sum = x + y
+print(sum)
+print("-----------------------------------------")
+
+# Calculate the square of a number and print the result.
+
+print("square of 2:", 2**2)
+print("-----------------------------------------")
+
+# Find the remainder when 27 is divided by 4.
+
+print("Remainder 27 / 4 :", 27 % 4)
+print("-----------------------------------------")
+
+# Convert the float 3.75 into an integer.
+
+print(int(3.75))
+print("-----------------------------------------")
+
+# Convert the integer 7 into a float.
+
+print(float(7))
+print("-----------------------------------------")
+
+# Round the number 12.6789 to 2 decimal places.
+
+print("Round:", round(12.6789, 2))
+print("-----------------------------------------")
+
+# Find the absolute value of -23.4.
+
+print("absolute:", abs(-23.4))
+print("-----------------------------------------")
+
+# Use divmod() to return the quotient and remainder of 20 divided by 3.
+
+print(divmod(20, 3))
+print("-----------------------------------------")
+
+# Multiply a float by an integer: 3.5 × 4.
+
+print(3.6 * 4)
+print("-----------------------------------------")
+
+# Use exponentiation to calculate 5 raised to the power of 3.
+
+print(5**3)
+print("-----------------------------------------")
+
+# 💡 Real-World Applications
+
+# Calculate the area of a circle with radius 5. Use πr².
+
+radius = 5
+pi = 3.14
+
+area = pi * (radius**2)
+print(area)
+print("-----------------------------------------")
+
+# Convert a temperature of 100°C to Fahrenheit using the formula:
+# F = (C × 9/5) + 32
+
+celcius = 100
+temp_in_celcius = (celcius * (9 / 5)) + 32
+print(temp_in_celcius)
+print("-----------------------------------------")
+
+# Calculate the monthly interest on a loan of GHS 2000 at an annual interest rate of 12%.
+
+monthly_interest = 2000 * (12 / 100)
+print(monthly_interest)
+print("-----------------------------------------")
+
+# You have $200 and want to split it evenly among 3 friends.
+
+# How much does each get?
+
+print(200 // 3)
+print("-----------------------------------------")
+
+# What is the remainder?
+
+print(200 % 3)
+print("-----------------------------------------")
+
+# A car travels 120 km in 2.5 hours.
+
+# What is its average speed?
+
+print(120 * 2.5)
